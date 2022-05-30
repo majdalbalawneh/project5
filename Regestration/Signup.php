f33c53bd7234d17eadd0fcfb4f384a6a4e82ec07
majdalbalawneh <majd3180157@gmail.com>
2022-05-31 01:05:36 +0300

    p

--- Regestration/Signup.php
+++ Regestration/Signup.php
@@ -1,28 +1,37 @@
 <?php
 session_start();
      include_once '../Configration/connection.php';
-    
+    if (isset($_POST['submit'])){
+
      $reg_name="/^([a-zA-Z' ]+)$/";
      $reg_PhoneNum="/^\\(?([0-9]{3})\\)?[-.\\s]?([0-9]{3})[-.\\s]?([0-9]{4})?[-.\\s]?([0-9]{4})$/";
      $reg_email="/^[a-zA-Z0-9][\-_\.\+\!\#\$\%\&\'\*\/\=\?\^\`\{\|]{0,1}([a-zA-Z0-9][\-_\.\+\!\#\$\%\&\'\*\/\=\?\^\`\{\|]{0,1})*[a-zA-Z0-9]@[a-zA-Z0-9][-\.]{0,1}([a-zA-Z][-\.]{0,1})*[a-zA-Z0-9]\.[a-zA-Z0-9]{1,}([\.\-]{0,1}[a-zA-Z]){0,}[a-zA-Z0-9]{0,}$/i";
-     $firstName_done= $SecName_done= $LastName_done= $DateOfBirt_done= $Number_done= $Email_done= $Pass_done= $ConfirmPassword_done="";
+     $firstName_done= $LastName_done=  $Number_done= $Email_done= $Pass_done= $ConfirmPassword_done="";
 
-     $sql= " SELECT * FROM register ;";
+
+
+     $fname=$_POST['First_Name'];
+    //  $sname=$_POST['Sec_Name'];
+     $lname=$_POST['Last_Name'];
+    //  $dob  =$_POST['DOB'];
+     $phone=$_POST['Phone_Num'];
+     $email=$_POST['Email'];
+    // $pass=password_hash($_POST['Password'],PASSWORD_DEFAULT);
+    $pass=$_POST['Password'];
+    $con_pass=$_POST['Con_Password'];
+     
+    
+    
+    
+    $sql= " SELECT * FROM register ;";
      $result= mysqli_query($conn,$sql);
     $result_check= mysqli_num_rows($result);
     if ($result_check > 0) {
        
       while ($row=mysqli_fetch_assoc($result)) {
 
-     if (isset($_POST['submit'])){
-     $fname=$_POST['First_Name'];
-     $sname=$_POST['Sec_Name'];
-     $lname=$_POST['Last_Name'];
-     $dob  =$_POST['DOB'];
-     $phone=$_POST['Phone_Num'];
-     $email=$_POST['Email'];
-     $pass=$_POST['Password'];
-     $con_pass=$_POST['Con_Password'];
+     
+     
 
 
             if(preg_match($reg_name,$fname)){
@@ -33,13 +42,13 @@ session_start();
                 $firstName_done=false;
             }
 
-            if(preg_match($reg_name,$sname)){
-                $SecName_check="<small style='color:white'> ✅ Correct Name </small><br>";
-                $SecName_done=true;
-            }else{
-                $SecName_check="<br><small style='color:white'>❌ Incorrect Name</small><br>";
-                $SecName_done=false;
-            }
+            // if(preg_match($reg_name,$sname)){
+            //     $SecName_check="<small style='color:white'> ✅ Correct Name </small><br>";
+            //     $SecName_done=true;
+            // }else{
+            //     $SecName_check="<br><small style='color:white'>❌ Incorrect Name</small><br>";
+            //     $SecName_done=false;
+            // }
 
             if(preg_match($reg_name,$lname)){
                 $LastName_check="<small style='color:white'>✅ Correct Name </small><br>";
@@ -49,14 +58,14 @@ session_start();
                 $LastName_done=false;
             }
 
-            if((floor((time() - strtotime($dob)) / 31556926)) <16){
-                $DateOfBirt_check="<span style='color:red ;font-family:Chaparral Pro Light;'>❌ You Are Too Young To Register ! </small><br>";
-                $DateOfBirt_done=false;
+            // if((floor((time() - strtotime($dob)) / 31556926)) <16){
+            //     $DateOfBirt_check="<span style='color:red ;font-family:Chaparral Pro Light;'>❌ You Are Too Young To Register ! </small><br>";
+            //     $DateOfBirt_done=false;
         
-            }else{
-                $DateOfBirt_check="<small style='color:white'>✅ Your age is Legal to Register </small><br>";
-                $DateOfBirt_done=true;
-            }
+            // }else{
+            //     $DateOfBirt_check="<small style='color:white'>✅ Your age is Legal to Register </small><br>";
+            //     $DateOfBirt_done=true;
+            // }
 
 
             if(preg_match($reg_PhoneNum,$phone)){
@@ -101,9 +110,9 @@ session_start();
             }}}
         
 
-     if($firstName_done && $SecName_done && $LastName_done && $DateOfBirt_done && $Number_done && $Email_done && $ConfirmPassword_done && $Pass_done ){
-        $sql= " INSERT INTO register(First_Name, Sec_Name, Last_Name ,DOB, Phone_Num,  Email , Password, Con_Password)
-        VALUES ('$fname', '$sname','$lname', '$dob' ,  '$phone','$email', '$pass' , '$con_pass');";
+     if($firstName_done &&  $LastName_done && $Number_done && $Email_done && $ConfirmPassword_done && $Pass_done ){
+        $sql= " INSERT INTO register(First_Name,  Last_Name , Phone_Num,  Email , Password, Con_Password)
+        VALUES ('$fname', '$lname',   '$phone','$email', '$pass' , '$con_pass');";
 
         if(mysqli_query($conn, $sql)){
         header("location:../Login/Login.php");
@@ -124,65 +133,100 @@ session_start();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.14.0/css/all.css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <link rel="stylesheet" href="signup.css">
-    
+    <!-- <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.14.0/css/all.css"> -->
+    <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"> -->
+    <!-- <link rel="stylesheet" href="signup.css"> -->
+    <style>
+      body{
+  background:url('../img/login.png') no-repeat , linear-gradient(to right,#A0BCC2,  #DAE5D0 , #FEFBE7 , #FFCAC2);  
+  background-size: 100% 140% ;   }
 
+ .login-cont{
+   border: 2px #000 double;
+   width: 26%;
+   background-color:rgba(255, 255, 255, 0.4);
+   margin-left: 60%;
+   margin-top: 10%;
+   padding: 20px 50px 50px ;
+   border-top-right-radius: 30px;
+   border-bottom-left-radius: 30px;
+ }
+
+ .form-inputt{border-radius: 10px;
+ border: none;
+ border-bottom: gray 1px solid;
+font-size: 20px;
+padding: 10px;}
+
+.bttn-login{border-radius: 10px;
+width: 60%;
+font-size: x-large;
+font-family: 'Courier New', Courier, monospace;
+background: #C3DBD9;
+border: 1px #8FBDD3 solid ;}
+
+.ll{color: #e55951;
+font-size: xx-large;}
+
+.bttn-login:hover{background: #8FBDD3;}
+    </style>
     <title>Register</title>
 </head>
 <body>
+  <div class="login-cont">
   <form  method="POST">    
-<div style="margin-top=5%" id="wrap">
+<!-- <div style="margin-top=5%" id="wrap">
   <div class="form">
     <div class="form-content">
-      <div class="form-container">
-        <span class="form-header">SIGNUP</span>
-      </div>
+      <div class="form-container"> -->
+        <h1 class="form-header"><strong class="ll">S</strong>ign up</h1>
         <div class="form-container">
         <div class="case">
         First Name<br>
-          <input style="height:30px" class="form-input"  type="text" name="First_Name" value="<?php if(isset($fname))echo $fname ?>" ><br>
+          <input  class="form-inputt"  type="text" name="First_Name" value="<?php if(isset($fname))echo $fname ?>" ><br><br>
           <?php if(isset($firstName_check)){echo $firstName_check;} ?><br><br>
 
         </div>
       </div>
 
-      <div style="margin-top:-5%" class="form-container">
+      <!-- <div style="margin-top:-5%" class="form-container">
         <div class="case">
         Secound Name<br>
-          <input style="height:30px" class="form-input"  type="text" name="Sec_Name" value="<?php if(isset($sname))echo $sname ?>"><br><?php if(isset($SecName_check)){ echo $SecName_check; } ?> <br><br>
+          <input  class="form-inputt"  type="text" name="Sec_Name" value="<?php #if(isset($sname))echo $sname ?>"><br><?php #if(isset($SecName_check)){ echo $SecName_check; } ?> <br><br>
 
         </div>
-      </div>
+      </div> -->
 
       <div style="margin-top:-5%" class="form-container">
         <div class="case">
         Last Name<br>
-          <input style="height:30px" class="form-input"  type="text" name="Last_Name" value="<?php if(isset($lname))echo $lname ?>" ><br><?php if(isset($LastName_check)){ echo $LastName_check; } ?> <br><br>
+          <input  class="form-inputt"  type="text" name="Last_Name" value="<?php if(isset($lname))echo $lname ?>" ><br><br>
+          <?php if(isset($LastName_check)){ echo $LastName_check; } ?> <br><br>
 
         </div>
       </div>
 
-      <div style="margin-top:-5%" class="form-container">
+      <!-- <div style="margin-top:-5%" class="form-container">
         <div class="case">
         Date Of Birth<br>
-          <input style="height:30px" class="form-input"  type="Date" name="DOB" value="<?php if(isset($dob))echo $dob ?>" >
-          <br><?php if(isset($DateOfBirt_check)){ echo $DateOfBirt_check; } ?><br><br>
+          <input  class="form-inputt"  type="Date" name="DOB" value="<?php #if(isset($dob))echo $dob ?>" >
+          <br><?php #if(isset($DateOfBirt_check)){ echo $DateOfBirt_check; } ?><br><br>
         </div>
-      </div>
+      </div> -->
 
       <div style="margin-top:-5%" class="form-container">
         <div class="case">
         Phone Number<br>
-          <input style="height:30px" class="form-input" type="Number" name="Phone_Num" value="<?php if(isset($phone))echo $phone ?>" ><br><?php if(isset($Number_check)){echo $Number_check;}?><br><br>
+          <input  class="form-inputt" type="Number" name="Phone_Num" value="<?php if(isset($phone))echo $phone ?>" ><br><br>
+          <?php if(isset($Number_check)){echo $Number_check;}?><br><br>
         </div>
       </div>
 
       <div style="margin-top:-5%" class="form-container">
         <div class="case">
         Email<br>
-          <input style="height:30px" class="form-input" type="email" name="Email" value="<?php if(isset($email)){echo $email;} ?>" ><br><?php if(isset($Email_check)){echo $Email_check;}?><br><br>
+          <input  class="form-inputt" type="email" name="Email" value="<?php if(isset($email)){echo $email;} ?>" ><br><br>
+          <?php if(isset($Email_check)){echo $Email_check;}?><br><br>
 
         </div>
       </div>
@@ -190,34 +234,32 @@ session_start();
       <div style="margin-top:-5%" class="form-container">
         <div class="case">
         Password<br>
-          <input style="height:30px" class="form-input" type="password" name="Password">
-          <br><?php if(isset($Pass_Check)){echo $Pass_Check;}?><br>
+          <input  class="form-inputt" type="password" name="Password">
+          <br><?php if(isset($Pass_Check)){echo $Pass_Check;}?><br><br>
         </div>  
       </div>
 
       <div style="margin-top:-5%" class="form-container">
         <div class="case">
         Confirm Password<br>
-          <input style="height:30px" class="form-input" type="password" name="Con_Password">
+          <input  class="form-inputt" type="password" name="Con_Password">
           <br><?php if(isset($ConfirmPassword_Check)){echo $ConfirmPassword_Check;}?>
         </div>
       </div>
 
-
+<br><br>
       <div class="form-add-text">
         <span >Already have an account ! <a style="text-decoration:none ; color:#e55951" href='../Login/Login.php'>login</span>
       </div>
       <div class="form-button">
-        <button class="btn-login" name="submit" >
+        <button class="bttn-login" name="submit" >
           <span class="btn-text">Signup</span>
         </button>
       </div>
     </div>
-  </div>
-  </div>
-</div>
+
 
     </form>
-
+</div>
 </body>
 </html>
